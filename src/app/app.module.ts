5927c4f07a2461828dbf0859d8a813e352ef2ee2
scaicedo27 <45157348+scaicedo27@users.noreply.github.com>
2020-03-18 04:50:19 -0500

    Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
